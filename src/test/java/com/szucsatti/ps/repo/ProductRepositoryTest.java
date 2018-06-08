d1e5ec1235c8f4879863f3d8dfac6906751f0e63
unknown <szucsatti@gmail.com>
2018-06-08 15:51:14 +0300

    import.sql lines counted automatically.

--- src/test/java/com/szucsatti/ps/repo/ProductRepositoryTest.java
+++ src/test/java/com/szucsatti/ps/repo/ProductRepositoryTest.java
@@ -2,6 +2,13 @@ package com.szucsatti.ps.repo;
 
 import static org.junit.Assert.assertThat;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import javax.annotation.Resource;
 
 import org.hamcrest.Matchers;
@@ -13,19 +20,31 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.szucsatti.ps.config.DataSourceConfig;
-import com.szucsatti.ps.repo.ProductRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { DataSourceConfig.class }, loader = AnnotationConfigContextLoader.class)
 @Transactional
 public class ProductRepositoryTest {
 
+	private static final String IMPORT_FILE = "./src/test/resources/import.sql";
+	
 	@Resource
 	private ProductRepository productRepo;
 
 	@Test
-	public void givenProductRepo_assertTwo() {
-		assertThat(productRepo.findAll().size(), Matchers.is(23));
+	public void givenProductRepo_assertTwo() throws FileNotFoundException, IOException {
+		Integer lineCount= countLinesInFile(IMPORT_FILE);
+		assertThat(productRepo.findAll().size(), Matchers.is(lineCount));
+	}
+
+	private Integer countLinesInFile(final String fileName) throws IOException, FileNotFoundException {
+		Integer lineCount;
+		try (LineNumberReader count = new LineNumberReader(new FileReader(fileName))) {
+			while (count.skip(Long.MAX_VALUE) > 0) {
+			}
+			lineCount = count.getLineNumber() + 1;
+		}
+		return lineCount;
 	}
 
 }
